6b5d75d61505eddf2a12c0fa2adfa638a08bce71
IviIvo <97630515+IviIvo@users.noreply.github.com>
2023-11-14 13:39:25 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    music.ringTone(262)
+})
+input.onButtonPressed(Button.AB, function () {
+    music.stopAllSounds()
+})
+input.onButtonPressed(Button.B, function () {
+    music.ringTone(440)
+})
